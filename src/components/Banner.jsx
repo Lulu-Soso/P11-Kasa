9ed49c99d6d275efc54abaf3e6c60cfe143c9b8e
Lulu-Soso <lucas.sokha@gmail.com>
2023-05-27 14:50:56 +0200

    add style in banner and navigation

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const Banner = () => {
   return (
       <div className="banner">
+        <div className="banner-cover"></div>
         <h1>Chez vous, partout et ailleurs</h1>
       </div>
   );
